041ed6910ba488f95e89d09a4d617cbf72d0bb50
VishalVinayRam <vishalvinayram5432@gmail.com>
2022-03-11 22:49:58 +0530

    deployment-2

--- diabates/settings.py
+++ diabates/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz9x#@s1u7dg1#cm*&_cesct^j9jmb6(0b=6=l24te&xm29343'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diabates-predictor.azurewebsites.net/']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
